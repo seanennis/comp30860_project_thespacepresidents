54466ab52f3516f71fab529cbbea5f53e8caa0be
Luke Murphy <luke.murphy3@ucdconnect.ie>
2020-03-06 16:00:10 +0000

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 INSERT INTO users(name, email, password, dob, islibrarian) VALUES('Luke Murphy', 'murphyluke01@gmail.com', 'password', '22/04/1999', FALSE);
 
 INSERT INTO artifacts(name, type, onLoan, owner, reserved, renewed, reserver, datecreated, dateexpires) VALUES('Harry Potter 1', 'Book', TRUE, 1, FALSE, FALSE, FALSE, '04/03/2020', '11/03/2020');
@@ -8,7 +7,7 @@ INSERT INTO artifacts(name, type, onLoan, owner, reserved, renewed, reserver, da
 INSERT INTO artifacts(name, type, onLoan, owner, reserved, renewed, reserver, datecreated, dateexpires) VALUES('Harry Potter 5', 'Movie', FALSE, null, FALSE, FALSE, FALSE, '04/03/2020', '11/03/2020');
 INSERT INTO artifacts(name, type, onLoan, owner, reserved, renewed, reserver, datecreated, dateexpires) VALUES('Harry Potter 6', 'Book', FALSE, null, FALSE, FALSE, FALSE, '04/03/2020', '11/03/2020');
 INSERT INTO artifacts(name, type, onLoan, owner, reserved, renewed, reserver, datecreated, dateexpires) VALUES('Harry Potter 7', 'Movie', FALSE, null, FALSE, FALSE, FALSE, '04/03/2020', '11/03/2020');
-=======
+
 INSERT INTO users(name, email, password, dob, islibrarian) VALUES('Luke Murphy', 'murphyluke01@gmail.com', 'p', '22/04/1999', FALSE);
 INSERT INTO users(name, email, password, dob, islibrarian) VALUES('Luke Murphy', 'murphyluke01@gmail.com', 'p', '22/04/1999', FALSE);
 INSERT INTO users(name, email, password, dob, islibrarian) VALUES('Luke Murphy', 'murphyluke01@gmail.com', 'p', '22/04/1999', FALSE);
@@ -28,4 +27,3 @@ INSERT INTO artifacts(artifact_id, name, type, onLoan, owner, reserved, reserver
 INSERT INTO artifacts(artifact_id, name, type, onLoan, owner, reserved, reserver) VALUES(4, 'Harry Potter 4', 'Movie', FALSE, null, FALSE, null);
 INSERT INTO artifacts(artifact_id, name, type, onLoan, owner, reserved, reserver) VALUES(7, 'Harry Potter 7', 'Book', FALSE, null, FALSE, null);
 INSERT INTO artifacts(artifact_id, name, type, onLoan, owner, reserved, reserver) VALUES(6, 'Harry Potter 6', 'Book', FALSE, null, FALSE, null);*/
->>>>>>> db2c90c3abc7d95c5274f995c8ba22f1270c4813
