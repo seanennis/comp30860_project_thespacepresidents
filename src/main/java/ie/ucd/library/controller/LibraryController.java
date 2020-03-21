c91219829f27bf077c94ae116c9cb3682280c5f6
Luke Murphy <luke.murphy3@ucdconnect.ie>
2020-03-21 00:14:54 +0000

    fixed merge conflict

--- src/main/java/ie/ucd/library/controller/LibraryController.java
+++ src/main/java/ie/ucd/library/controller/LibraryController.java
@@ -88,16 +88,10 @@ public class LibraryController {
     public void registerPost(@RequestParam(name="name") String name, @RequestParam(name="password") String password, @RequestParam(name="email") String email,
      @RequestParam(name="dob") String dob, HttpServletResponse response) throws Exception {
         User newUser = new User(name, password, email, dob);
-        if((newUser.getEmail().contains("@")) && (newUser.getEmail().contains(".")))
-        {
         userRepository.save(newUser);
         session.setUser(newUser);
         sendEmail(email, newUser.getId());
         response.sendRedirect("/user");
-        }
-        else{
-            response.sendRedirect("/register");
-        }
     }
 
     @GetMapping("/user")
@@ -224,6 +218,7 @@ public class LibraryController {
 
     @GetMapping("/loanHistory")
     public String getLoanHistory(Model model) {
+        /*List<Artifact> artifacts = artifactRepository.findAll();*/
         List<Loan> loans = loanRepository.findByOwner(session.getCurrentUser().getId());
         List<Loan> loanHistory = new ArrayList<>();
         for(Loan loan : loans) {
@@ -236,6 +231,7 @@ public class LibraryController {
 
     @GetMapping("/userLoanHistory")
     public String getUserLoanHistory(@RequestParam(name="id") int id, Model model) {
+        /*List<Artifact> artifacts = artifactRepository.findAll();*/
         List<Loan> loans = loanRepository.findByOwner(id);
         List<Loan> loanHistory = new ArrayList<>();
         for(Loan loan : loans) {
@@ -309,23 +305,18 @@ public class LibraryController {
         response.sendRedirect("/search?search="+search);
     }
 
-    @GetMapping("/userID")
-    public String userID(@RequestParam(name="id") int id, HttpServletResponse response, Model model) throws Exception {
+    @GetMapping("/saveArtifactID")
+    public void saveArtifactID(@RequestParam(name="search") String search, @RequestParam(name="id") int id, HttpServletResponse response) throws Exception {
         this.takeOutArtifactID = id;
-
-        return "userID.html";
-    }
-
-    @GetMapping("/userIDReserve")
-    public String userIDReserve(@RequestParam(name="id") int id, HttpServletResponse response, Model model) throws Exception {
-        this.takeOutArtifactID = id;
-
-        return "userIDReserve.html";
+        System.out.println(id);
+        response.sendRedirect("/search?search="+search);
     }
 
     @GetMapping("/takeOutUserLoan")
     public void takeOutUserLoan(@RequestParam(name="id") String userID, HttpServletResponse response) throws Exception {
         int id = Integer.parseInt(userID);
+        System.out.println(id);
+        System.out.println(this.takeOutArtifactID);
         Optional<User> userOptional = userRepository.findById(id);
         User user = userOptional.get();
         Optional<Artifact> artifactOptional = artifactRepository.findById(this.takeOutArtifactID);
@@ -333,6 +324,7 @@ public class LibraryController {
         if(artifactOptional.isPresent()) {
             Artifact artifact = artifactOptional.get();
             artifact.setOwner(user.getId());
+            System.out.println(artifact.getId());
             artifact.setOnLoan(true);
             artifact.setDateCreated(loanDate.getLoanDate());
             artifact.setDateExpires(loanDate.getReturnDate());
@@ -364,49 +356,26 @@ public class LibraryController {
         response.sendRedirect("/search?search="+search);
     }
 
-    @GetMapping("/reserveForUser")
-    public void reserveForUser(@RequestParam(name="id") int id, HttpServletResponse response) throws Exception {
-        Optional<Artifact> artifactOptional = artifactRepository.findById(this.takeOutArtifactID);
-        if(artifactOptional.isPresent()) {
-            Artifact artifact = artifactOptional.get();
-            artifact.setReserver(id);
-            artifact.setReserved(true);
-            artifactRepository.save(artifact);
-        }
-        response.sendRedirect("/search?search="+this.currentSearch);
-    }
-
     @GetMapping("/returnLoan")
-<<<<<<< HEAD
     public void returnLoan(@RequestParam(name="id") int id, HttpServletResponse response) throws Exception {
         Optional<Artifact> artifactOptional = artifactRepository.findById(id);
         List<Artifact> artifacts = artifactRepository.findByOwner(session.getCurrentUser().getId());
-=======
-    public void returnLoan(@RequestParam(name="id") String id, HttpServletResponse response) throws Exception {
-        Optional<Artifact> artifactOptional = artifactRepository.findById(Integer.parseInt(id));
-        Integer userID = null;
->>>>>>> 8f5b313b3d971898b576a9a61a8bace719e9e40b
         if(artifactOptional.isPresent()) {
             Artifact artifact = artifactOptional.get();
-            List<Loan> loans = loanRepository.findByArtifactIDAndOwner(Integer.parseInt(id), artifact.getOwner());
+            List<Loan> loans = loanRepository.findByArtifactIDAndOwner((int) id, (Integer) artifact.getOwner());
             for(Loan loan : loans) {
-                if(loan.getActive())
+                if(loan.getActive()) {
                     loan.setActive(false);
                     loanRepository.save(loan);
                     break;
+                }
             }
-            userID = artifact.getOwner();
             artifact.setOnLoan(false);
             artifact.setReserver(null);
             artifact.setReserved(false);
             artifact.setOwner(null);
             artifactRepository.save(artifact);
-
         }
-         if(session.getCurrentUser().isLibrarian() && userID != null) {
-           response.sendRedirect("/viewUserLoansString?id="+userID.intValue());
-        }
-        else 
         response.sendRedirect("/viewLoans");
     }
 
@@ -439,10 +408,17 @@ public class LibraryController {
     void removeDuplicates(List<Artifact> artifacts, boolean loggedIn) {
         int i;
 
+        /*for(Artifact a : artifacts)
+            System.out.println(a.getName() + ", " + a.getOnLoan() + ", " + a.getOwner());
+        System.out.println();*/
+
         if(loggedIn) {
             for(i = 0;i < artifacts.size()-1;i++) {
                 Integer owner = artifacts.get(i).getOwner();
                 Integer reserver = artifacts.get(i).getReserver();
+      
+                /*System.out.println("i: " + i);
+                System.out.println("Owner: " + owner + ", reserver: " + reserver);*/
 
                 boolean ownerFound = false;
                 boolean reserverFound = false;
@@ -469,9 +445,17 @@ public class LibraryController {
             }
         }
 
+
+        /*for(Artifact a : artifacts)
+            System.out.println(a.getName() + ", " + a.getOnLoan());
+        System.out.println();*/
+
         for(i = artifacts.size()-1;i > 0;i--)
             if(artifacts.get(i).getArtifactId() == artifacts.get(i - 1).getArtifactId())
                 artifacts.remove(i - 1);
 
+        /*for(Artifact a : artifacts)
+            System.out.println(a.getName() + ", " + a.getOnLoan());
+        System.out.println();*/
     }
 }
